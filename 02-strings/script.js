64bc2a61e7994a3b7f7d5762e400b42dcc3d4c15
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-14 15:53:46 +0100

    [UPD]string 1 2 3

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,12 +1,19 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+let name = 'Ilaria';
 // Stampa il valore di nome nella console.
-
+console.log(name);
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+let number;
+let text;
+let bool;
 // un numero, una stringa e un valore booleano.
+number= 5;
+text= 'pippo';
+bool= 'true';
 // Stampa tutti i valori nella console.
-
+console.log(number, text, bool);
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
@@ -14,7 +21,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = `${firstName} ${lastName}`;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
